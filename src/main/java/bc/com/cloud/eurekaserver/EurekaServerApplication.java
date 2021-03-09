a1e0d5dd71c33fdab6ec729f70096e7eef8bfee5
钟哲 <f7k_h3xpl9oo1@dingtalk.com>
2021-03-09 20:35:40 +0800

    zz

---
+++ src/main/java/bc/com/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package bc.com.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableConfigServer
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
